dad41f1090c719be2423ad24b0e23a8d46b5d775
omarguesmi <guesmiomarog@gmail.com>
2025-07-31 01:49:06 +0100

    add appdownload component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Home from './pages/home/Home';
 import PlaceOrder from './pages/PlaceHolder/PlaceOrder';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
 const App = () => {
   return (
+    <>
     <div>
       <Navbar/>
       <Routes>
@@ -15,6 +17,9 @@ const App = () => {
         <Route path='/cart' element={<PlaceOrder/>} /> 
       </Routes>
     </div>
+    <Footer></Footer>
+    </>
+    
   )
 }
 
